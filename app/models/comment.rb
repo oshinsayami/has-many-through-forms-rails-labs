e5a0ce99bff712ccbaace0e38be907f07bfad381
Simran <mdrsimran@gmail.com>
2021-01-08 10:22:21 -0600

    commit tree 13e1664ab4c386541263eb1da9782bc84886f90b

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ActiveRecord::Base
 
   accepts_nested_attributes_for :user
 
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
 end
